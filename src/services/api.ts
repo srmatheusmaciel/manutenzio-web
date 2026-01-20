ffe1d59c8c3bc918069ad52434e498eb1acb5044
Matheus Maciel <srmatheusmaciel@hotmail.com>
2026-01-20 12:20:22 -0300

    configuracao de variavel de ambiente para api url

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
+const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:8080';
+
 export const api = axios.create({
-    baseURL: 'http://localhost:8080/api',
+    baseURL: API_URL, 
 });
 
 
